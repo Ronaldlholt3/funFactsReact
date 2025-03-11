282d27a8b885df162a832888a2a2313ded63cae8
Ronnie <ronaldlholt3@gmail.com>
2025-03-11 13:33:03 -0400

    phase 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/funFactsReact">
+    <Router basename="/funFactsReact"> 
       {/* <BrowserRouter basename="/funFactsReact"> */}
       <div className="app-container">
         <nav>
